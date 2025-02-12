502217831983435e13c0d1ad6f26e548bd53b63a
Youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-12 17:21:56 +0100

    encrypt password when sending form

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  name: process.env.DB_NAME
+  database: process.env.DB_NAME
 })
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','ejs');
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/',(req,res)=>{
     res.send('Hi from Home');
@@ -15,6 +21,22 @@ app.get('/dbtest', async(req,res)=>{
     } catch(err) {
         console.error(err);
         res.status(500).send('Database error');
+    }
+})
+
+app.get('/signup', (req,res)=>{
+    res.render('form');
+})
+
+app.post('/register', async(req,res)=>{
+    const {username, password} = req.body;
+    const hachedPassword = await bcrypt.hash(password,10);
+    try{
+        await db.query('INSERT INTO users (username,password) VALUES ($1, $2)',[username,hachedPassword]);
+        res.status(200).send('User registed');
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Failed to register the user');
     }
 })
 
