a98d8e943e4fc007420d66bdb25bb9f028013823
Youssef <115870740+youssefbnncr@users.noreply.github.com>
2025-02-12 16:22:10 +0100

    connect db test

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('Hi from Home');
+})
+
+app.get('/dbtest', async(req,res)=>{
+    try {
+        const result = await db.query('SELECT now()');
+        console.log(result.rows);
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Database error');
+    }
 })
 
 const PORT = process.env.PORT || 3000
